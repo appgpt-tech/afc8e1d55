1ed509e80cb8bc7a151632ac9fb03410aee95219
11ai <124818466+11ai@users.noreply.github.com>
2024-01-31 14:06:15 +0200

    created on Wed Jan 31 2024 12:06:04 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/dashboard.tsx
@@ -0,0 +1,7 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+    export const Dashboard = () => (
+      <Card>
+      <CardHeader title= "Game Management System" />
+      <CardContent> A system to manage players, games, animations, colors and lights. < /CardContent>
+      </Card>
+    );
